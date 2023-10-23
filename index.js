cf9489a781b892a9d73720abd414c4a005644ae9
iftyzubaer <iftyzubaer1304.com>
2023-10-23 17:05:41 +0600

    displayed the data through database to UI

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gsq0j5x.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://TechyMaster:jjvWrPpUVuqHuuZ1@cluster0.gsq0j5x.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,6 +22,21 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const productCollection = client.db('productDB').collection('product')
+
+        app.get('/product', async(req, res) => {
+            const cursor = productCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.post('/product', async(req, res) => {
+            const newProduct = req.body
+            console.log(newProduct);
+            const result = await productCollection.insertOne(newProduct)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
